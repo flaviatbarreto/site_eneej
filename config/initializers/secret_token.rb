f8fcc6cfaf3e9b9645bbd6cd1c8a4507adc996c9
Danilo Rocha <danilo.ras@gmail.com>
2011-10-21 11:11:16 -0300

    Aqui começa uma nova geração

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteEneej::Application.config.secret_token = '96043feb5e652e8b38177d1656248734b551cad1b6dffe13c7ac20510ad7af7c556d120bd58b512fedb664be135cf4af0540d87fbbfa129f67c18b197e5a01d8'
